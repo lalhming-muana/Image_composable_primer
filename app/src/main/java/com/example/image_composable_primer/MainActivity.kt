729f6b45905fc0097f538ee143cad2a8ba552908
lhmuana <edd_vai@yahoo.co.in>
2023-10-05 00:46:18 +0530

    A image is presented

--- app/src/main/java/com/example/image_composable_primer/MainActivity.kt
+++ app/src/main/java/com/example/image_composable_primer/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.image_composable_primer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.image_composable_primer.ui.theme.Image_composable_primerTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +40,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Column {
+
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+
+    Image(
+        painter = painterResource(R.drawable.lola),
+        contentDescription = null,
+        modifier = modifier
+            .padding(6.dp)
+            .size(dimensionResource(R.dimen.image_size))
+            .clip(MaterialTheme.shapes.small),
+        contentScale = ContentScale.Crop
+    )
+    }
 }
 
 @Preview(showBackground = true)
